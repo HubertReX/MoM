b4ec97da6dc7360ac326de657e44b3aced1468e2
Hubert Nafalski <hubert.nafalski@gmail.com>
2026-07-26 08:58:00 +0200

    added beds tileset

---
+++ project/assets/NinjaAdventure/maps/tilesets/Beds.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Beds" tilewidth="16" tileheight="16" tilecount="168" columns="14" objectalignment="topleft">
+ <image source="images/TilesetBed.png" trans="000000" width="224" height="192"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="yellow_vertiacl_bed"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="yellow_vertiacl_bed"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="yellow_horizontal_bad"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="yellow_horizontal_bad"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" value="yellow_horizontal_bad"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="name" value="yellow_vertiacl_bed"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="name" value="yellow_vertiacl_bed"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="name" value="yellow_horizontal_bad"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="name" value="yellow_horizontal_bad"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="name" value="yellow_horizontal_bad"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="name" value="yellow_vertiacl_bed"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="name" value="yellow_vertiacl_bed"/>
+  </properties>
+ </tile>
+</tileset>
